dd580d230b24d8e27b1c12db1271ee19e841b30f
Mo.Rehan <morehanp@gmail.com>
2020-10-12 13:07:31 +0600

    App is Ready to Be Deploy

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,6 +29,32 @@ const todoController = (function () {
       return allToDos.ToDos.splice(idIdxOf, 1);
     },
 
+    updateToDoToDS(ID, value) {
+      allToDos.ToDos.forEach((todo) => {
+        if (todo.id === parseInt(ID)) {
+          todo.value = value;
+        }
+      });
+    },
+
+    moveToDoToCompleted(id) {
+      let completedToDo;
+      allToDos.ToDos.forEach((todo, idx) => {
+        if (todo.id === parseInt(id)) {
+          completedToDo = allToDos.ToDos.splice(idx, 1);
+        }
+      });
+
+      allToDos.completed.push(completedToDo);
+      return completedToDo[0].id;
+    },
+
+    removeCompletedToDoFromDS(ID) {
+      let idArr = allToDos.completed.map((todo) => todo.id);
+      let idIdxOf = idArr.indexOf(ID);
+      return allToDos.completed.splice(idIdxOf, 1);
+    },
+
     testing() {
       return allToDos;
     },
@@ -43,8 +69,12 @@ const UIController = (function () {
     toDoParentElement: ".to-do-list__body",
     removeToDoLabel: ".to-do-list__delete",
     deleteIcon: ".to-do-list__icon--small--delete",
+    editIcon: ".to-do-list__icon--small--edit",
     toDoItem: ".to-do-list__item",
     toDoBody: ".to-do-list__body",
+    toDoTextBox: ".to-do-list__text-box",
+    toDoInput: ".to-do-list__input",
+    completedBody: ".completed__body",
   };
 
   return {
@@ -90,6 +120,59 @@ const UIController = (function () {
       document.getElementById(`to-do__${id}`).remove();
     },
 
+    editToDoToUI(toDoTextBox) {
+      let editInput = document.createElement("input");
+      editInput.classList.add("to-do-list__input");
+      editInput.setAttribute("type", "text");
+      editInput.value = toDoTextBox.firstElementChild.innerText;
+
+      toDoTextBox.parentElement.replaceChild(editInput, toDoTextBox);
+      editInput.select();
+      toDoTextBox.remove();
+    },
+
+    updateToDoToUI(toDoInput) {
+      let toDoTextBox = document.createElement("div");
+      toDoTextBox.classList.add("to-do-list__text-box");
+      let toDoText = document.createElement("p");
+      toDoText.classList.add("to-do-list__item--text");
+      toDoText.textContent = toDoInput.value;
+
+      toDoTextBox.appendChild(toDoText);
+      toDoInput.parentElement.replaceChild(toDoTextBox, toDoInput);
+    },
+
+    modifyTickIconBox(e) {
+      if (e.target.className === "fa fa-check to-do-list__icon--medium") {
+        e.target.parentElement.classList.add(
+          "to-do-list__icon-box--body--active"
+        );
+      } else if (e.target.className === "to-do-list__icon-box--body") {
+        e.target.classList.add("to-do-list__icon-box--body--active");
+      }
+    },
+
+    markAsCompletedToDo(completedToDoId) {
+      let completedToDo = document.getElementById(`to-do__${completedToDoId}`);
+      let completedToDoValue = completedToDo.textContent;
+      let completedStructure = `
+      <div class="completed__item" id="completed__${completedToDoId}">
+        <p class="completed__item--text">${completedToDoValue}</p>
+        <div class="completed__icon-box--body">
+          <i class="fa fa-trash completed__icon--small--delete" aria-hidden="true"></i>
+        </div>
+      </div>
+      `;
+      completedToDo.remove();
+      document.querySelector(
+        DOMStrings.completedBody
+      ).innerHTML += completedStructure;
+    },
+
+    removeCompletedToDoFromUI(ID) {
+      document.getElementById(`completed__${ID}`).remove();
+    },
+
     getDOMStrings() {
       return DOMStrings;
     },
@@ -101,13 +184,43 @@ const appController = (function (todoCtrl, UICtrl) {
   const DOMStrings = UICtrl.getDOMStrings();
 
   const ctrlEventHandler = function () {
+    //? Add To Do Event Listener
     document
       .querySelector(DOMStrings.addBtn)
       .addEventListener("click", addToDo);
 
     document
+      .querySelector(DOMStrings.addInput)
+      .addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+          addToDo();
+        }
+      });
+
+    //? Remove To Do Event Listener
+    document
       .querySelector(DOMStrings.toDoBody)
       .addEventListener("click", removeToDo);
+
+    //? Edit To Do Event Listener
+    document
+      .querySelector(DOMStrings.toDoBody)
+      .addEventListener("click", editToDo);
+
+    //? Update To Do Event Listener
+    document
+      .querySelector(DOMStrings.toDoBody)
+      .addEventListener("keypress", updateToDo);
+
+    //? Mark As Completed  To Do Event Listener
+    document
+      .querySelector(DOMStrings.toDoBody)
+      .addEventListener("click", markAsCompleted);
+
+    // Remove Completed To Do Event Listener
+    document
+      .querySelector(DOMStrings.completedBody)
+      .addEventListener("click", removeCompletedToDo);
   };
 
   const addToDo = function () {
@@ -118,23 +231,84 @@ const appController = (function (todoCtrl, UICtrl) {
     let toDoID = todoCtrl.addToDoToDS(toDo);
 
     // 3. Update the UI
-    UICtrl.addToDoToUI(toDo, toDoID);
+    if (toDo) UICtrl.addToDoToUI(toDo, toDoID);
   };
 
   const removeToDo = function (e) {
     if (e.target.className === "fa fa-trash to-do-list__icon--small--delete") {
-      let toBeRemovedToDo = document
-        .querySelector(DOMStrings.removeToDoLabel)
-        .closest(DOMStrings.toDoItem);
+      let toBeRemovedToDo = e.target.parentElement.parentElement;
 
       // 2. Grab its ID
       let id = toBeRemovedToDo.id.split("__")[1];
 
       // 3. Remove ToDo from Data Structure
-      todoCtrl.removeToDoFromDS(id);
+      todoCtrl.removeToDoFromDS(parseInt(id));
 
       // 4. Remove ToDo from the UI
       UICtrl.removeToDoFromUI(id);
+    }
+  };
+
+  const editToDo = function (e) {
+    if (
+      e.target.className ===
+      "fa fa-pencil to-do-list__icon--small to-do-list__icon--small--edit"
+    ) {
+      let eventTarget = e.target;
+      let toDoTextBox = eventTarget.parentElement.previousElementSibling;
+
+      // Update the UI & Show Input Box
+      UICtrl.editToDoToUI(toDoTextBox);
+    }
+  };
+
+  const updateToDo = function (e) {
+    if (e.key === "Enter") {
+      let toDoInput = document.querySelector(DOMStrings.toDoInput);
+      let id = toDoInput.parentElement.id;
+      let idNum = id.split("__")[1];
+
+      // 1. Update the To Do In Data Structure
+      todoCtrl.updateToDoToDS(idNum, toDoInput.value);
+
+      // 2. Update The UI
+      UICtrl.updateToDoToUI(toDoInput);
+    }
+  };
+
+  const markAsCompleted = function (e) {
+    if (
+      e.target.className === "fa fa-check to-do-list__icon--medium" ||
+      e.target.className === "to-do-list__icon-box--body"
+    ) {
+      let completedToDo =
+        e.target.className === "fa fa-check to-do-list__icon--medium"
+          ? e.target.parentElement.parentElement
+          : e.target.parentElement;
+
+      // 1. Modify Tick Icon Box
+      UICtrl.modifyTickIconBox(e);
+
+      // 2. Update the DS
+      let completedId = todoCtrl.moveToDoToCompleted(
+        completedToDo.id.split("__")[1]
+      );
+
+      // 3. Update The Ui
+      UICtrl.markAsCompletedToDo(completedId);
+    }
+  };
+
+  const removeCompletedToDo = function (e) {
+    if (e.target.className === "fa fa-trash completed__icon--small--delete") {
+      let completedToDoID = e.target.parentElement.parentElement.id.split("__")[1];
+
+      console.log(completedToDoID);
+      // 1. Remove Completed To Do From DS
+      todoCtrl.removeCompletedToDoFromDS(parseInt(completedToDoID));
+
+      // 2. Remove Completed To Do From UI
+      UICtrl.removeCompletedToDoFromUI(completedToDoID);
     }
   };
 
